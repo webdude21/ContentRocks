f1be425cdb50ba9addc2a3a895d494f01fc2c4d4
webdude <webdude@webdude.eu>
2015-05-03 16:13:48 +0300

    Further removing magic strings from the project

---
+++ Web.Infrastructure/Constants/Resources.cs
@@ -0,0 +1,29 @@
+﻿namespace Web.Infrastructure.Constants
+{
+    public class ClientResources
+    {
+        public const string JQueryValidation = "~/bundles/jqueryval";
+
+        public const string JQueryValidationPath = "~/Scripts/jquery.validate*";
+
+        public const string JQuery = "~/bundles/jquery";
+
+        public const string JQueryPath = "~/Scripts/jquery-{version}.js";
+
+        public const string Modernizr = "~/bundles/modernizr";
+
+        public const string ModernizrPath = "~/Scripts/modernizr-*";
+
+        public const string Bootstrap = "~/bundles/bootstrap";
+
+        public const string BootstrapStylesPath = "~/Content/bootstrap.css";
+
+        public const string BootstrapPath = "~/Scripts/bootstrap.js";
+
+        public const string RespondPath = "~/Scripts/respond.js";
+
+        public const string SiteCssPath = "~/Content/site.css";
+
+        public const string Css = "~/Content/css";
+    }
+}
--- Web/Config/BundleConfig.cs
+++ Web/Config/BundleConfig.cs
@@ -2,22 +2,24 @@
 {
     using System.Web.Optimization;
 
+    using Web.Infrastructure.Constants;
+
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle(ClientResources.JQuery).Include(ClientResources.JQueryPath));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle(ClientResources.JQueryValidation).Include(ClientResources.JQueryValidationPath));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle(ClientResources.Modernizr).Include(ClientResources.ModernizrPath));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle(ClientResources.Bootstrap).Include(ClientResources.BootstrapPath, ClientResources.RespondPath));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle(ClientResources.Css).Include(ClientResources.BootstrapStylesPath, ClientResources.SiteCssPath));
         }
     }
 }
