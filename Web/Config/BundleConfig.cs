dc16cf8b4272bffec90550a7420de0c74d4d9e10
webdude <webdude@webdude.eu>
2015-05-04 23:06:09 +0300

    Fixed TinyMce

--- Web/Config/BundleConfig.cs
+++ Web/Config/BundleConfig.cs
@@ -8,6 +8,17 @@
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            RegisterScripts(bundles);
+
+            RegisterStyles(bundles);
+
+            bundles.Add(new StyleBundle(ClientResources.Css).Include(ClientResources.BootstrapStylesPath, ClientResources.SiteCssPath));
+
+            BundleTable.EnableOptimizations = true;
+        }
+
+        public static void RegisterScripts(BundleCollection bundles)
+        {
             bundles.Add(new ScriptBundle(ClientResources.JQuery).Include(ClientResources.JQueryPath));
 
             bundles.Add(new ScriptBundle(ClientResources.JQueryValidation).Include(ClientResources.JQueryValidationPath));
@@ -15,7 +26,10 @@
             bundles.Add(new ScriptBundle(ClientResources.Modernizr).Include(ClientResources.ModernizrPath));
 
             bundles.Add(new ScriptBundle(ClientResources.Bootstrap).Include(ClientResources.BootstrapPath, ClientResources.RespondPath));
+        }
 
+        public static void RegisterStyles(BundleCollection bundles)
+        {
             bundles.Add(new StyleBundle(ClientResources.Css).Include(ClientResources.BootstrapStylesPath, ClientResources.SiteCssPath));
         }
     }
