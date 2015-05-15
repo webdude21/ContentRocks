50b8f250a0fc5236de95f2319093fe9d36e2f782
Dimo Petrov <dimo.petrov@sap.com>
2015-05-15 15:01:18 +0300

    GlobalConfig

---
+++ Web.Infrastructure/Constants/ImageRoute.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Web.Infrastructure.Constants
+{
+    using Config;
+
+    public static class ImageRoute
+    {
+        public const string DefaultController = Controllers.Images;
+
+        public const string Detail = Actions.GetImage;
+
+        public const string FileNameMatcher = @"\S+";
+
+        public const string Name = "Images";
+
+        public static string UrlPattern = GlobalConstants.PostImagesRelativePath.Substring(1) + "{fileName}";
+    }
+}
