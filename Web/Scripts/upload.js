adc299ac5897106da0d4030377b253eb53b99baa
Dimo Petrov <dimo.petrov@sap.com>
2015-05-29 19:03:06 +0300

    add additional scripts

---
+++ Web/Scripts/upload.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#add-input-file').on('click', function () {
+        var form = $('#form-upload');
+
+        var count = form.find('input[type="file"]').length;
+
+        var $divContainer = $('<div class="form-group">');
+        var $inputContainer = $('<div class="col-md-10">');
+        var inputTypeFile = $('<input class="form-control" type="file" name="file' + count + '" id="file_' + count + '" />');
+
+        $inputContainer.append(inputTypeFile);
+        $divContainer.append($inputContainer);
+
+        form.append($divContainer);
+    });
+});
