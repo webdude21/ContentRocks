db2fed131a263771c74c4b5e635ca2da33e1f23b
Димо <webdude@webdude.eu>
2016-08-05 06:31:35 +0300

    config changes

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -26,33 +26,35 @@ namespace Data.Migrations
             var posts = context.Set<Post>();
             var pages = context.Set<Page>();
 
-            if (!posts.Any())
+            if (posts.Any())
             {
-                var dataGenerator = new ContentFactory(RandomDataGenerator.Instance);
-                var category = dataGenerator.GetCategory(3);
-                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-                var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-                roleManager.Create(new IdentityRole(GlobalConstants.AdministratorRoleName));
-
-                var author = new ApplicationUser
-                                 {
-                                     Email = GlobalConstants.DefaultLoginEmail,
-                                     UserName = GlobalConstants.DefaultUserName
-                                 };
-
-                userManager.Create(author, author.UserName);
-                userManager.AddToRole(author.Id, GlobalConstants.AdministratorRoleName);
-                category.Author = author;
-
-                var post = dataGenerator.GetRealisticPost();
-                var page = dataGenerator.GetAboutPage();
-
-                post.Author = author;
-                post.Category = category;
-                post.Comments = dataGenerator.GetComments(42, post, author);
-                pages.Add(page);
-                posts.Add(post);
+                return;
             }
+
+            var dataGenerator = new ContentFactory(RandomDataGenerator.Instance);
+            var category = dataGenerator.GetCategory(3);
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            roleManager.Create(new IdentityRole(GlobalConstants.AdministratorRoleName));
+
+            var author = new ApplicationUser
+                             {
+                                 Email = GlobalConstants.DefaultLoginEmail,
+                                 UserName = GlobalConstants.DefaultUserName
+                             };
+
+            userManager.Create(author, author.UserName);
+            userManager.AddToRole(author.Id, GlobalConstants.AdministratorRoleName);
+            category.Author = author;
+
+            var post = dataGenerator.GetRealisticPost();
+            var page = dataGenerator.GetAboutPage();
+
+            post.Author = author;
+            post.Category = category;
+            post.Comments = dataGenerator.GetComments(42, post, author);
+            pages.Add(page);
+            posts.Add(post);
         }
     }
 }
